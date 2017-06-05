ed8ff80a6b20d1592c519485a6f9f9aac1cc4ddc
Victor Igor <victor.0w3@hotmail.com>
2017-06-04 23:19:46 -0300

    creating cards in local, module ask

--- src/content/Ask.js
+++ src/content/Ask.js
@@ -1,10 +1,23 @@
-import { getInLocal } from '../popup/store/LocalStore.js';
-import { getIndexThingById } from '../popup/reducers/stateManipulate.js';
+import { getInLocal, saveInLocal } from '../popup/store/LocalStore';
+import { getIndexThingById } from '../popup/reducers/stateManipulate';
+import { getRandomInt } from '../shared/helpers';
+import { dec } from 'ramda';
 
-export const ask = async () => {
+export const loadPacks = async () => {
   const allPacks = await getInLocal('packState');
   const idPackToTrain = await getInLocal('idPackToTrain');
   const indexOfThePack = getIndexThingById(allPacks, idPackToTrain);
-  const packToTrain = allPacks[indexOfThePack];
-}
+  const packInTraining = allPacks[indexOfThePack];
+  // saveInLocal('cardsInTraining', packInTraining.cards);
+};
 
+const getRandomCard = (cards) => {
+	const indexCardBeingUsed = getRandomInt(0, dec(cards.length));
+  console.log('indexBeingUsed: ', indexCardBeingUsed);
+	return cards[indexCardBeingUsed];
+};
+
+export const ask = async () => {
+  const cardsInTraining = await getInLocal('cardsInTraining');
+  const card = getRandomCard(cardsInTraining);
+};
--- src/content/index.js
+++ src/content/index.js
@@ -1,7 +1,8 @@
-import { ask } from './Ask.js';
+import { ask, loadPacks } from './Ask.js';
 import 'babel-polyfill';
 
-ask();
+loadPacks().then(() => ask());
+
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.message && (msg.message == "LOAD")) {
 			console.log('loadQuestion....');
