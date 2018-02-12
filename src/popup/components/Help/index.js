e800e3d2e63176e98f9e9b58c432997d035a809f
Victor Igor <victor.0w3@hotmail.com>
2018-02-12 18:45:57 -0200

    fixed tooltip and help tooltip added

--- src/popup/components/Help/index.js
+++ src/popup/components/Help/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Tooltip from 'components/Tooltip'
+import * as translator from 'shared/constants/internacionalization'
 
 const handleClick = () => {
   chrome.tabs.create({url: 'https://fluany.com/help'});
@@ -7,6 +9,7 @@ const handleClick = () => {
 const Help = () => (
   <section className="help-content">
     <a href="#" onClick={handleClick}>?</a>
+    <Tooltip name={translator.CONTENT_HELP} />
   </section>
 )
 
--- src/shared/constants/internacionalization.js
+++ src/shared/constants/internacionalization.js
@@ -54,4 +54,4 @@ export const CONTENT_ANSWER_BUTTON = chrome.i18n.getMessage('contentAnswerButton
 export const CONTENT_FEEDBACK_SUCCESS = chrome.i18n.getMessage('contentFeedbackSuccess')
 export const CONTENT_NEXT_QUESTION = chrome.i18n.getMessage('contentNextQuestion')
 export const CONTENT_THE_CORRECT_ANSWER_LABEL = chrome.i18n.getMessage('contentTheAnswerIs')
-
+export const CONTENT_HELP = chrome.i18n.getMessage('contentHelp')
